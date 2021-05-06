4d7d444c718b64e79262637cdd5ae6dc80e807a6
radhika011 <60853138+radhika011@users.noreply.github.com>
2021-05-06 12:27:25 +0530

    Update RateFilter.java

--- RateFilter.java
+++ RateFilter.java
@@ -30,34 +30,34 @@ public  class RateFilter implements Filter{
 	   
 	   public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterchain) 
 	      throws IOException, ServletException {
-		   System.out.println("Do filter invoked!");
+		   //System.out.println("Do filter invoked!");
 		   HttpServletRequest httpsrequest = (HttpServletRequest) request;
 	       HttpServletResponse httpsresponse = (HttpServletResponse) response;
 		   RateLimiter limiter;
-		   String current_ip = request.getRemoteAddr();
-		   if(dictionary.containsKey(current_ip)) {
-			   limiter= dictionary.get(current_ip);
+		   String current_ip = request.getRemoteAddr();   //gets ip address of the client
+		   if(dictionary.containsKey(current_ip)) {        //if it is not a new client
+			   limiter= dictionary.get(current_ip);      //get the limiter object assigned to that ip address
 		   }
-		   else {
-			   limiter = new RateLimiter();
-			   dictionary.put(current_ip, limiter);
+		   else {                                             //if it is a new client
+			   limiter = new RateLimiter();             //create a new limiter object
+			   dictionary.put(current_ip, limiter);      //store it in the dictionary
 		   }
 	      //System.out.println("Remote Host:"+request.getRemoteHost());
-	      System.out.println("Remote Address:"+request.getRemoteAddr());
-	      System.out.println(limiter.availableTokens);
-	      if(limiter.tryConsume()==true) {
-	    	  filterchain.doFilter(request, response);
+	      //System.out.println("Remote Address:"+request.getRemoteAddr());
+	      //System.out.println(limiter.availableTokens);
+	      if(limiter.tryConsume()==true) {                    //if tokens are available to be consumed, allow the request to pass through
+	    	  filterchain.doFilter(request, response);         
 	      }
-	      else {
-	    	  httpsresponse.sendError(HttpStatus.TOO_MANY_REQUESTS.value());
+	      else {                                                    
+	    	  httpsresponse.sendError(HttpStatus.TOO_MANY_REQUESTS.value());  //else, respond with a 429 error
 	      }
-	      System.out.println("Do filter done!");
+	      //System.out.println("Do filter done!");
 		   
 	   }
 	   @Override
 	   public void init(FilterConfig filterconfig) throws ServletException {
-		   System.out.println("Init invoked!");
-		   dictionary = new HashMap<String,RateLimiter>();
+		   //System.out.println("Init invoked!");
+		   dictionary = new HashMap<String,RateLimiter>();         //creates a dictionary object when filter is placed into service
 	   }
 
 
