b64dbb725f250a24382a55ed6150533f8f28d7fe
radhika011 <60853138+radhika011@users.noreply.github.com>
2021-05-09 13:39:13 +0530

    Update PrincipalController.java

--- PrincipalController.java
+++ PrincipalController.java
@@ -1,56 +1,51 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import java.util.*;
 import javax.servlet.http.*;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
 import java.io.*;
 import org.springframework.stereotype.Component;
 @RestController
 
 public class PrincipalController {
-	ParkingLot pl1 = new ParkingLot();
+	@Autowired
+	ParkingLot pl1;
 	@RequestMapping()
 	String sayHello() {
 		return "Hello World!";
 	}
-	@RequestMapping(value="/park/id")
-	String park(@RequestParam("id") int carID,HttpServletResponse response) throws IOException{
-		Car car1 = new Car(carID);
+	@RequestMapping(value="/parking-lot/car/car-id/{car_id}/park")
+	String park(@PathVariable int car_id,HttpServletResponse response) throws IOException{
+		Car car1 = new Car(car_id);
 		int slotNo = car1.park(pl1);
 		if(slotNo==-1) {
 			response.sendError(HttpStatus.BAD_REQUEST.value());
 		}
-		return  "Car "+carID+" parked at "+slotNo;
+		return  "Car "+car_id+" parked at "+slotNo;
 	}
-	@RequestMapping(value="/unpark/slot")
-	String unpark(@RequestParam("slot") int slotNo,HttpServletResponse response) throws IOException{
-		Car unparked = pl1.unPark(slotNo);
+	@RequestMapping(value="/parking-lot/slot/slot-no/{slot_no}/unpark")
+	String unpark(@PathVariable int slot_no,HttpServletResponse response) throws IOException{
+		Car unparked = pl1.unPark(slot_no);
 		if(unparked==null) {
 			response.sendError(HttpStatus.BAD_REQUEST.value());
 		}
-		return "Car with car ID "+unparked.getCarID()+" unparked from "+slotNo;
+		return "Car with car ID "+unparked.getCarID()+" unparked from "+slot_no;
 	}
-	@RequestMapping(value="/getBySlot/slot",method = RequestMethod.GET)
-	String showCarID(@RequestParam("slot") int slotNo) {
-		int carID = pl1.getBySlot(slotNo);
-		return "Car with ID: "+carID+" at slot number:"+slotNo;
+	@RequestMapping(value="/parking-lot/slot/slot-no/{slot_no}",method = RequestMethod.GET)
+	String showCarID(@PathVariable int slot_no) {
+		int carID = pl1.getBySlot(slot_no);
+		return "Car with ID: "+carID+" at slot number:"+slot_no;
 	}
-	@RequestMapping(value="/getByCarID/carID",method = RequestMethod.GET)
-	String showSlotNo(@RequestParam("carID") int carID) {
-		return "Car with ID: "+carID+" at slot number:"+pl1.getByCarID(carID);
+	@RequestMapping(value="/parking-lot/car/car-id/{car_id}",method = RequestMethod.GET)
+	String showSlotNo(@PathVariable int car_id) {
+		return "Car with ID: "+car_id+" at slot number:"+pl1.getByCarID(car_id);
 	}
 }
+
