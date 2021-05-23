0fe6ff9e4e2d4a07101b068fe52a81eeee29722d
radhika011 <60853138+radhika011@users.noreply.github.com>
2021-05-23 23:17:31 +0530

    Delete AppConfig.java

--- AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-import org.springframework.context.annotation.*;
-@Configuration
-public class AppConfig {
-	@Bean(initMethod = "init")
-	ParkingLot myParkingLot() {
-		return new ParkingLot();
-	}
-}
